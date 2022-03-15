67054f34fe9287464a2cc948b584a58d13f48a4b
Jeyi Adole <erthnice@gmail.com>
2022-03-15 14:05:10 +0100

    NavBar Mobile

--- 03-ecommerce-website/src/Pages/Navbar.js
+++ 03-ecommerce-website/src/Pages/Navbar.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import { BsCartPlusFill } from 'react-icons/bs';
+import React, { useRef } from 'react';
+import { BsCartPlusFill, BsTi } from 'react-icons/bs';
+import { FaTimes, FaBars } from 'react-icons/fa';
+
 import { useGlobalContext } from '../context';
 
 const Navbar = () => {
   const { cart, amount } = useGlobalContext();
+  const ulContainer = useRef(null);
   return (
-    <header>
-      <nav className='navbar'>
-        <div className='title'>logo</div>
-        <div className='cart-icon'>
-          <p>{amount}</p>
-          <BsCartPlusFill />
+    <header className='container'>
+      <nav>
+        <p className='title'>Deseo</p>
+        <div className='sub-nav'>
+          <ul ref={ulContainer}>
+            <span
+              className='close-btn'
+              onClick={() => {
+                ulContainer.current.classList.remove('visible');
+              }}
+            >
+              <FaTimes />
+            </span>
+            <li>
+              <a href='#'> Collections</a>
+            </li>
+            <li>
+              <a href='#'> Men</a>
+            </li>
+            <li>
+              <a href='#'> Women</a>
+            </li>
+            <li>
+              <a href='#'> Jewelry</a>
+            </li>
+          </ul>
+          <span
+            className='btn-bars'
+            onClick={() => {
+              ulContainer.current.classList.add('visible');
+            }}
+          >
+            <FaBars />
+          </span>
+          <p className='cart-icon'>
+            <span>{amount}</span>
+            <BsCartPlusFill />
+          </p>
         </div>
       </nav>
     </header>
