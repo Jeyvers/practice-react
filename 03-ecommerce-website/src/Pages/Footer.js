ffe702e58a68d9a0355de502e58c5e9f990dfc8b
Jeyi Adole <erthnice@gmail.com>
2022-03-18 16:35:54 +0100

    Added Social Media links to footer

--- 03-ecommerce-website/src/Pages/Footer.js
+++ 03-ecommerce-website/src/Pages/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='footer '>
-      <div>
+      <div className='info'>
         <h1>Jeyvers</h1>
 
         <p>
@@ -15,9 +16,32 @@ const Footer = () => {
           <a href='https://jeyversportfolio.netlify.app/'> Jeyi Adole </a>
         </p>
       </div>
+      <div className='social-media'>
+        <a
+          href='https://linkedin.com/in/jeyi-adole-1301401b9'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href='https://twitter.com/the_erthnite?s=09'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href='https://www.github.com/jeyvers'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaGithub />
+        </a>
+      </div>
       <span>
         UI designed and developed by Jeyvers. Web Application built with React
-        solely for pratice and showcase purposes.{' '}
+        solely for pratice and showcase purposes.
       </span>
     </footer>
   );
