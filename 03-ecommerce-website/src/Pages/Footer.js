34760250e8801958a9ff5e36218c31a7dedd4235
Jeyi Adole <erthnice@gmail.com>
2022-03-18 16:14:39 +0100

    Footer CSS

--- 03-ecommerce-website/src/Pages/Footer.js
+++ 03-ecommerce-website/src/Pages/Footer.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='footer container'>
+    <footer className='footer '>
       <div>
         <h1>Jeyvers</h1>
 
-        <p>API consumed: </p>
-        <p>Jeyi's portfolio:</p>
+        <p>
+          API consumed :
+          <a href=' https://fakestoreapi.com/ '> Fake Store API</a>
+        </p>
+        <p>
+          Jeyi's portfolio :
+          <a href='https://jeyversportfolio.netlify.app/'> Jeyi Adole </a>
+        </p>
       </div>
-      <p>
+      <span>
         UI designed and developed by Jeyvers. Web Application built with React
         solely for pratice and showcase purposes.{' '}
-      </p>
+      </span>
     </footer>
   );
 };
